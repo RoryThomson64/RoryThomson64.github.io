ba336641daf784cd58c992f6076b826748a63313
Rory Thomson <rthomson700@gmail.com>
2024-03-16 20:22:38 +1300

    Removed experiment

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,10 +1,8 @@
 import { Details } from "./Details"
-import { Experiment } from "./Experiment"
 import { Intro } from "./Intro"
 
 export const Home = () => {
     return <>
-        <Experiment />
         <Intro />
         <Details />
     </>
