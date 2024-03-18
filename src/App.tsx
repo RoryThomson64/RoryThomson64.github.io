b26c885240cf057bb4ac515b6a2392704912050c
Rory Thomson <rthomson700@gmail.com>
2024-03-19 10:51:56 +1300

    Added temp Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { NavBar } from './Components/NavBar'
 import { Home } from './Components/Home'
+import { Contact } from './Components/Contact'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<div>Projects</div>} />
-        <Route path="/contacts" element={<div>contacts</div>} />
+        <Route path="/contact" element={<Contact />} />
 
       </Routes>
       <NavBar />
---
+++ src/Components/Contact.tsx
@@ -0,0 +1,7 @@
+export const Contact = () => {
+    return (<>
+        <h1>
+            Work In Progress, Please Check Back here!
+        </h1>
+    </>)
+}
