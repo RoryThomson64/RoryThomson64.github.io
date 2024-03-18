7a9cd4ab8447776e0b53a56786611109a6f36a8a
Rory Thomson <rthomson700@gmail.com>
2024-03-19 10:47:50 +1300

    Changed Hero summary

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -5,7 +5,13 @@ export const Intro = () => {
 
     const style = useAppSelector((state) => state.style)
     // const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante semper, suscipit nulla eu, venenatis purus. Vestibulum ultrices vel enim id eleifend. Aliquam vitae luctus sem. Ut et lectus gravida, luctus nibh vel, rutrum odio. Morbi varius varius diam nec egestas. Vestibulum blandit varius bibendum. Sed vestibulum ex dui, finibus tincidunt magna accumsan et. Praesent eget eleifend lorem, ut faucibus arcu. Mauris sapien ante, lacinia id commodo vel, dignissim eget erat. Vivamus finibus vestibulum placerat.\n\rMaecenas varius cursus nisl, vel feugiat ligula pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi et diam sem. Pellentesque at elementum sapien, vel rhoncus augue. Morbi nec pharetra massa. Nulla blandit consectetur libero, ac eleifend mauris ultricies vitae. Praesent eget sapien feugiat, maximus tortor vel, fermentum tellus. Quisque risus diam, lobortis vel nulla quis, efficitur mattis risus. Fusce imperdiet augue quis mattis scelerisque. Donec et libero nulla. Quisque venenatis viverra eros. Suspendisse tincidunt, tortor sit amet consequat egestas, dolor augue ultricies lorem, sit amet tincidunt justo neque a lorem."
-    const summaryText = "Passionate and innovative Software Developer dedicated to creating novel solutions and finding satisfaction in overcoming complex challenges. With a relaxed and conscientious demeanor, I prioritize collaboration and empathy in my work. I thrive on the excitement of problem-solving and deeply value the well-being of others. Let's collaborate on groundbreaking projects and make a positive impact together.";
+    const summaryText = `
+    Welcome! I'm an innovative software developer passionate about solving complex problems.
+    With a knack for detail and a love for creativity, I thrive on pushing boundaries in technology.
+    I excel in crafting elegant code, architecting robust systems, and exploring cutting-edge technologies.
+    Explore my portfolio to see how we can collaborate on turning your ideas into reality.
+    `;
+
     return (
         <>
             <style>
